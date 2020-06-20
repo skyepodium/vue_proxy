2d6bbd327a709b8307604d73a905d77bb1a12058
skyepodium <skyepodium@gmail.com>
2020-06-20 16:58:20 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    main
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  created() {
+    // 1. 브라우저에서 직접 요청
+    this.corsRequest() 
+
+    // 2. 개발 서버를 이용해서 프록시 요청
+    this.proxyRequest()
+  },
+  methods: {
+    corsRequest() {
+      axios.get("http://localhost:3000/api/getList")
+      .then((res) => {
+        console.log('corsRequest res', res)
+      })
+      .catch((error) => {
+        console.log('corsRequest error', error)
+      })
+    },
+    proxyRequest() {
+      axios.get("/api/getList")
+      .then((res) => {
+        console.log('proxyRequest res', res)
+      })
+      .catch((error) => {
+        console.log('proxyRequest error', error)
+      })
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
